e28db990fafb6c609b7d9ad30457898058dc2de7
john alain <johnalain572@gmail.com>
2024-01-04 09:41:17 +0200

    create task

---
+++ async_create_task.py
@@ -0,0 +1,24 @@
+# import asyncio
+# from re import X
+import asyncio
+import time
+
+from asyncio1 import say_after
+
+async def main():
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
+
+    task2 = asyncio.create_task(
+         say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+# asyncio.run(main())
+    
