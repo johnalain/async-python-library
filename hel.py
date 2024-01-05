8a5cc3a2ea37791bed5d4ce6e0fe7772c228e36a
john alain <johnalain572@gmail.com>
2024-01-05 09:16:59 +0200

    operations in python

---
+++ hel.py
@@ -0,0 +1,60 @@
+# https://youtu.be/1cf6tUTGJQk
+#https://youtu.be/kA-3784mMBI constructor
+#https://youtu.be/eeRw__TlgmQ?list=PLlvFEn0NKwXIT9fR6gdZRcQSReKUpLWNz
+from math import *
+# x = []
+# help(x)
+# print(type(10))# class integer
+print(type(4+3j))#<class 'complex'>
+# print(25+'years')#you can not add int to string
+#convert int to string str(int)
+print(str (25)+' years')
+#<class 'complex'>
+#25 years
+print(str (25)+' '+ 'years')
+print(float(10))
+print(int(10.4))
+#output 10
+print(complex(10))
+#0utput (10+0j)
+print(round(7.5))
+#output 8
+print(round(7.3))
+# output 7
+# u must add math library "from math import *"
+print(floor(6.5))
+# output 6
+print(ceil(9.5))
+# output 10
+print(pow(2, 3))#thismeans 2**3
+# output 8 
+print(2**3)
+# output 8
+print(abs(-45))
+#output 45
+print(max(4,9))
+#output 9
+print(min(4,9))
+#output 4
+# name = input("enter your name: ")
+# print("you won! " + name)
+#enter your name: mike
+# you won! mike
+# name = input("enter your name: ")
+# age = input("Enter your age: ")
+# print("your age is: " + age)
+# print("you won! " + name)
+# enter your name: mike
+# Enter your age: 25css
+# your age is: 25
+# you won! mike
+x = input("Enter your lan1: ")
+z = input("Enter your lan2: ")
+print("i love : " + x)
+print("i love : " + z)
+
+
+
+
+
+
